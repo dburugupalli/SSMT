83a0dd580c3ca69154bdb4da8c9c89af79d1f74f
dburugupalli <burugupalli.d@husky.neu.edu>
2020-06-29 17:39:57 -0400

    User admin classification

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session,request,redirect,url_for
 from flask_cors import CORS
 import signal
 from flask import Flask
@@ -6,11 +6,43 @@ from routes.list_projects import list_projects
 from routes.list_runtime import list_runtime
 import sys
 import os
-
+import requests
+import constants
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(list_projects, url_prefix="/list_projects")
 app.register_blueprint(list_runtime, url_prefix="/list_runtime")
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        session.get('username')
+        return 'Logout using <a href="/logout">Logout</a><br> Login to Metering dashboard using <a href="http://0.0.0.0:9000">dashboard</a>' 
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'developer' and request.form['username'] == 'developer':
+        session['logged_in'] = True
+        session['username']  = request.form['username']
+        constants.username='developer'
+        # constants.username=request.form['username']
+    elif request.form['password'] == 'admin' and request.form['username'] == 'admin':  
+        session['logged_in'] = True
+        session['username']  = request.form['username']
+        constants.username='admin'
+    else:
+        print('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    session['username']  = None
+    constants.username = None
+    return home()
+
 
 def signal_term_handler(signal, frame):
     app.logger.warn('got SIGTERM')
@@ -18,4 +50,5 @@ def signal_term_handler(signal, frame):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGTERM, signal_term_handler)
+    app.secret_key = os.urandom(12)
     app.run(host=str(os.environ.get("HOST","localhost")), port=int(os.environ.get("PORT", 8000)))
--- routes/list_runtime.py
+++ routes/list_runtime.py
@@ -1,5 +1,6 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,session
 from flask import request
+import sqlite3 as sql
 from flask.json import jsonify
 import constants
 import json
@@ -28,8 +29,25 @@ def list_project_runtime_search(period_start, period_end):
     input_dict = json.loads(input_json)
     output_dict = [x for x in input_dict if x['period_start'] >= period_start]
     data = [x for x in output_dict if x['period_end'] <= period_end]
-    list_key_value = _countNumberOfHours(data)
+    output = list()
+    if constants.username == 'developer':
+        getProjects = getUserProjects()
+        for project in getProjects:
+            for x in data:
+                if project == x['namespace']:
+                    output.append(x)
+                else:
+                    continue
+        list_key_value = _countNumberOfHours(output)
+    else:
+        list_key_value = _countNumberOfHours(data)
+
     return jsonify(list_key_value)
+
+@list_runtime.route('/test')
+def test():
+    return jsonify(session.get('username'))
+
 
 def _countNumberOfHours(input_dict):
     ht=dict()
@@ -40,3 +58,16 @@ def _countNumberOfHours(input_dict):
             ht[x['namespace']] = 1 
     list_key_value = [ dict(namespace=k, activation_time=v) for k, v in ht.items() ]
     return list_key_value
+
+def getUserProjects():
+        con = sql.connect("sample.db")
+        con.row_factory = sql.Row
+        x=constants.username
+        cur = con.cursor()
+        cur.execute("SELECT DISTINCT project FROM user_projects WHERE name=?",(x,))
+        rows = cur.fetchall()
+            # return render_template("list.html",rows = rows)
+        projects = list()
+        for row in rows:
+            projects.append(row["project"])
+        return projects
